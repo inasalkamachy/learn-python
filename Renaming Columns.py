9b4d192daa71ad195090764e1c27fce760f50e77
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2021-06-21 10:40:52 +0300

    Update Renaming Columns.py

--- Renaming Columns.py
+++ Renaming Columns.py
@@ -6,3 +6,15 @@ df = pd.read_csv('imdb.csv')
 df.columns = ['ID', 'Title', 'Category', 'Year Released', 'Rating']
 
 print(df)
+
+
+#OR//
+
+df = pd.DataFrame({
+    'name': ['John', 'Jane', 'Sue', 'Fred'],
+    'age': [23, 29, 21, 18]
+})
+df.rename(columns={
+    'name': 'First Name',
+    'age': 'Age'},
+    inplace=True)
